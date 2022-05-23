f64c59bc87bae5681d210115e84dd13ed27fd6d2
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-05-23 17:31:35 -0400

    add comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,7 +3,13 @@ const { Pizza } = require('../models');
 const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
-      Pizza.find({})
+        Pizza.find({})
+        .populate({
+          path: 'comments',
+          select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
         .then(dbPizzaData => res.json(dbPizzaData))
         .catch(err => {
           console.log(err);
@@ -13,18 +19,22 @@ const pizzaController = {
   
     // get one pizza by id
     getPizzaById({ params }, res) {
-      Pizza.findOne({ _id: params.id })
+        Pizza.findOne({ _id: params.id })
+        .populate({
+            path: 'comments',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbPizzaData => {
-          // If no pizza is found, send 404
-          if (!dbPizzaData) {
+            if (!dbPizzaData) {
             res.status(404).json({ message: 'No pizza found with this id!' });
             return;
-          }
-          res.json(dbPizzaData);
+            }
+            res.json(dbPizzaData);
         })
         .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
+            console.log(err);
+            res.status(400).json(err);
         });
     },
 
