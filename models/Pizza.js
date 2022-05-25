b3aea6aff18b278ae1d20eec25ac3d888edb3f00
willduncan <willdunc12@gmail.com>
2022-05-25 16:11:48 -0400

    add validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,12 +3,16 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema(
     {
-      pizzaName: {
-        type: String
-      },
-      createdBy: {
-        type: String
-      },
+    pizzaName: {
+        type: String,
+        required: true,
+        trim: true
+        },
+        createdBy: {
+        type: String,
+        required: true,
+        trim: true
+    },
       createdAt: {
         type: Date,
         default: Date.now,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
       },
       size: {
         type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
       },
       toppings: [],
